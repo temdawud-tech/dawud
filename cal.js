90e755848923b8c7dd0cf96da8ff9910116a8c6f
dawudtem-pixel <dawudtem@gmail.com>
2025-12-06 17:17:52 +0300

    first commit

---
+++ cal.js
@@ -0,0 +1,15 @@
+//calculator
+const display = document.getElementById("display")
+function appendToDisplay(input){
+display.value +=input;
+}
+ function clearDisplay(){
+display.value = "";
+ }
+ function calculate(){
+display.value = eval(display.value)
+ }
+function deleteLast(){
+    display.value= display.value.slice(0,-1);
+}
+ 
